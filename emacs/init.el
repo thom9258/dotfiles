617eb2168b5fa2bb022cd3aeacadf4f256e85371
alexgaard <thomasjensen9258@gmail.com>
2023-10-03 15:09:38 +0200

    started rewriting the config

--- emacs/init.el
+++ emacs/init.el
@@ -11,22 +11,21 @@
 (require 'package)
 
 ;; Adds the Melpa archives to the list of available repositories
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;;(add-to-list 'package-archives
+;;            '("melpa" . "http://melpa.org/packages/") t)
+;;(add-to-list 'package-archives
+;;            '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-;; Initializes the package infrastructure
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ;;("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ;;("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ))
 (package-initialize)
 
-;; Download use-package to handle our packages instead of package-install
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package install 'use-package))
-(setq use-package-always-ensure t)
 
 ;; Load the config file
-(use-package org)
-;;(setq config-root "~/.emacs.d/")
-;;(org-babel-load-file (concat config-root "config.org"))
+(require 'org)
+;; Always follow symlinks for git sources (like this file!)
+(setq vc-follow-symlinks t)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
--- install.sh
+++ install.sh
@@ -5,14 +5,15 @@ DOT_DIR="$HOME/Dotfiles"
 
 echo "Installing Dotfiles from dir \"$DOT_DIR\""
 
-ln -sfn $DOT_DIR/emacs/init.el     $HOME/.emacs.d/init.el
-ln -sfn $DOT_DIR/emacs/config.org  $HOME/.emacs.d/config.org
-ln -sfn $DOT_DIR/emacs/starter.org $HOME/.emacs.d/starter.org
-ln -sfn $DOT_DIR/bash/bashrc.sh    $HOME/.bashrc
-ln -sfn $DOT_DIR/qtile             $HOME/.config/qtile
-ln -sfn $DOT_DIR/nvim              $HOME/.config/nvim
-ln -sfn $DOT_DIR/vim/vimrc         $HOME/.vimrc
-ln -sfn $DOT_DIR/zathura           $HOME/.config/zathura
+ln -sfn $DOT_DIR/emacs/init.el       $HOME/.emacs.d/init.el
+ln -sfn $DOT_DIR/emacs/early-init.el $HOME/.emacs.d/early-init.el
+ln -sfn $DOT_DIR/emacs/config.org    $HOME/.emacs.d/config.org
+ln -sfn $DOT_DIR/emacs/starter.org   $HOME/.emacs.d/starter.org
+ln -sfn $DOT_DIR/bash/bashrc.sh      $HOME/.bashrc
+ln -sfn $DOT_DIR/qtile               $HOME/.config/qtile
+ln -sfn $DOT_DIR/nvim                $HOME/.config/nvim
+ln -sfn $DOT_DIR/vim/vimrc           $HOME/.vimrc
+ln -sfn $DOT_DIR/zathura             $HOME/.config/zathura
 
 # Add bash helper scripts to path
 
