c833222f317eba53878db9a0abedb69611ffce01
Thomas Alexgaard <thomasjensen9258@gmail.com>
2024-09-13 16:49:33 +0200

    added error fix for installing packages such as magit

--- emacs/init.el
+++ emacs/init.el
@@ -6,92 +6,43 @@
 ;; so we can load our config.org properly.
 ;; ==============================================================================
 
-
-;; Enables basic packaging support
+;; -----------------------------------------------------------------------
+;; Enable Basic Packaging Support and Source Locations
 (require 'package)
 
-;; Adds the Melpa archives to the list of available repositories
-;;(add-to-list 'package-archives
-;;            '("melpa" . "http://melpa.org/packages/") t)
-;;(add-to-list 'package-archives
-;;            '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;; Add the Melpa archives to the list of available repositories
+(add-to-list 'package-archives
+            '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+            '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ;;("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ;;("melpa-stable" . "https://stable.melpa.org/packages/")
-                         ))
+;; -----------------------------------------------------------------------
+;; Install use-package as our primary package manager
 (package-initialize)
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
 
-
-;; Setup Elpaca package manager
-(defvar elpaca-installer-version 0.7)
-(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
-(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil
-                              :files (:defaults (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (order (cdr elpaca-order))
-       (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
-    (make-directory repo t)
-    (when (< emacs-major-version 28) (require 'subr-x))
-    (condition-case-unless-debug err
-        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                 ((zerop (call-process "git" nil buffer t "clone"
-                                       (plist-get order :repo) repo)))
-                 ((zerop (call-process "git" nil buffer t "checkout"
-                                       (or (plist-get order :ref) "--"))))
-                 (emacs (concat invocation-directory invocation-name))
-                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                 ((require 'elpaca))
-                 ((elpaca-generate-autoloads "elpaca" repo)))
-            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
-          (error "%s" (with-current-buffer buffer (buffer-string))))
-      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
-(add-hook 'after-init-hook #'elpaca-process-queues)
-(elpaca `(,@elpaca-order))
-
-
-;; Install use-package support
-(elpaca elpaca-use-package
-  ;; Enable :elpaca use-package keyword.
-  (elpaca-use-package-mode)
-  ;; Assume :elpaca t unless otherwise specified.
-  (setq elpaca-use-package-by-default t))
-
-;; Block until current queue processed.
-(elpaca-wait)
-
-;;Turns off elpaca-use-package-mode current declartion
-;;Note this will cause the declaration to be interpreted immediately (not deferred).
-;;Useful for configuring built-in emacs features.
-(use-package emacs :elpaca nil :config (setq ring-bell-function #'ignore))
-
-;; Don't install anything. Defer execution of BODY
-;;(elpaca nil (message "deferred"))
-
+;; -----------------------------------------------------------------------
 ;; Load the config file
-(require 'org)
-;; Always follow symlinks for git sources (like this file!)
-(setq vc-follow-symlinks t)
+(require 'org) ;; org is required for our configs
+(setq vc-follow-symlinks t) ;; Always follow symlinks for git sources (like this file!)
+
+;; TODO: we should split our config up into multiple modules to make things easier to read
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+
+
+
+
+;; -----------------------------------------------------------------------
+;; Random Shit Emacs Automatically Generates
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
+ '(package-selected-packages '(use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
